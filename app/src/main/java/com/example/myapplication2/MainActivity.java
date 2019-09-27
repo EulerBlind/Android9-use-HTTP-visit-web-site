97f2a2e6596393b7e12d0c0e9eef7157825d2d15
Elvis <1069772670@qq.com>
2019-09-27 15:14:37 +0800

    优化线程无节制创建

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -13,18 +13,25 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView content_tv;
+    Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,33 +48,89 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        content_tv = findViewById(R.id.main_tv);
+        if (null == timer) {
+            timer = new Timer();
+        }
 
-        Thread thread = new Thread() {
+        final Callback callback = new Callback() {
             @Override
-            public void run() {
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder()
-                        .get()
-                        .url("http:www.baidu.com")
-                        .build();
-                Call call = client.newCall(request);
-                try {
-                    Response response = call.execute();
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        content_tv.setText("获取数据失败！！！，请检查网络");
+                        Toast.makeText(MainActivity.this, "get failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
 
-                    Log.i("http-", response.toString());
-                    assert response.body() != null;
-                    String body = response.body().string();
-//
-                    Log.i("http-", body);
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String res = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        content_tv.setText(res);
+                    }
+                });
             }
         };
 
-        thread.start();
+
+        timer.schedule(new TimerTask() {
+            private OkHttpClient client = new OkHttpClient();
+
+            @Override
+            public void run() {
+                Request request = new Request.Builder()
+                        .get()
+                        .url("http:baidu.com")
+                        .build();
+                Call call = client.newCall(request);
+                Log.i("call", String.valueOf(call.isExecuted()));
+                Log.i("call", String.valueOf(call.isCanceled()));
+                call.enqueue(callback);
+                Log.i("ttt", "ttt");
+            }
+        }, 5, 5 * 10);
+
+
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//            }
+//        };
+//        runnable.run();
+
+//        Thread thread = new Thread() {
+//            @Override
+//            public void run() {
+//                OkHttpClient client = new OkHttpClient();
+//                Request request = new Request.Builder()
+//                        .get()
+//                        .url("http:127.0.0.1:5000")
+//                        .build();
+//                Call call = client.newCall(request);
+//
+//
+//                try {
+//                    Response response = call.execute();
+//
+//                    Log.i("http-", response.toString());
+//                    assert response.body() != null;
+//                    String body = response.body().string();
+////
+//                    Log.i("http-", body);
+//
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+
+//        thread.start();
 
 
     }
